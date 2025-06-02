112b9bce03fbe7ea2565e814e02b594368accdd2
Mohd-Arifgit config --global user.name Mohd-Arifgit config --global user.name Mohd-Arif <ariflucky1@gmail.com>
2025-06-02 19:52:34 +0530

    Nav Links and premium log

--- Jio-Cinema-UI/src/components/header/Header.jsx
+++ Jio-Cinema-UI/src/components/header/Header.jsx
@@ -1,24 +1,39 @@
 import styles from "./header.module.css";
-import JCLogo from "../../assets/jc_logo.png";
-import crown from "../../assets/crown.svg";
+import JCLogo from '../../assets/jc_logo.svg';
+import crown from '../../assets/crown.svg'
+
 
 export default function Header() {
+
+        let navLinks = ['Home', 'Sports', 'Movies', 'TV Shows', 'More']
+  
   return (
     <>
       <header className={styles.header}>
-        <nav className={styles.navigation}>
-         
+       
+       <nav className={styles.navigation}>
           <div className={styles.logo}>
-
-            <img src={JCLogo} alt="jioCinema logo" width="200px" height="35px"/>
+            <img src={JCLogo} alt="jc_logo" />
             <div className={styles.premium}>
-              <img src={crown} alt="crown" /> Go Premium</div>
+              <img src={crown} alt="crown" /> <p>Go Premium</p> </div>
+        </div>
 
-          </div>
+      <ul className={styles.navLinks}>
+            {navLinks.map((link)=>{
+              return <li className={styles.navLink}>{link}</li>
+            })
+            }
+      </ul>
 
-        </nav>
+       </nav>
 
-        <div className={styles.search}></div>
+        
+        <div className={styles.search}>
+
+            <div className={styles.searchBox}>
+
+            </div>
+        </div>
       </header>
     </>
   );
