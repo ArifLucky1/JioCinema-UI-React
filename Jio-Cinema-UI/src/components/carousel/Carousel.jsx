f68844a65c007872329ac01f60622f75bb14fa40
Mohd-Arifgit config --global user.name Mohd-Arifgit config --global user.name Mohd-Arif <ariflucky1@gmail.com>
2025-06-05 21:22:16 +0530

    Add Carousel

--- Jio-Cinema-UI/src/components/carousel/Carousel.jsx
+++ Jio-Cinema-UI/src/components/carousel/Carousel.jsx
@@ -1,11 +1,12 @@
 import styles from './carousel.module.css'
+import strange from '../../assets/strnge.jpg'
 
 const Carousel = () => {
   return (
     <>
     
         <section className={styles.carousel}>
-
+          <img src={strange} alt="poster" />
         </section>
     
     </>
