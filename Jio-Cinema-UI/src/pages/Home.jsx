0dbb6afc10fe9949023ab5583a2e0ad791d86da0
Mohd Arif <ariflucky1@gmail.com>
2026-07-16 20:25:58 +0530

    add backend (express) and movie data

--- Jio-Cinema-UI/src/pages/Home.jsx
+++ Jio-Cinema-UI/src/pages/Home.jsx
@@ -5,11 +5,29 @@ import Channels from "../components/channels/channels"
 import Carousel from "../components/carousel/Carousel"
 import Featured from "../components/featured/Featured"
 import Shows from "../components/shows/Shows"
+import { useEffect, useState } from "react"
 
 
 
 export default function Home() {
     
+      let [movies,setMovies] = useState([]);
+
+      useEffect(async () => {
+
+        try
+        {
+          let movieResponse = await fetch("http://localhost:3000/movies");
+          let moviesData = await movieResponse.json();
+          // console.log(movies);
+          setMovies(moviesData);
+        }
+        catch(err)
+        {
+          console.log(err);
+        }
+      }, [])
+
     return (
     <>
       <Header/>
@@ -18,8 +36,8 @@ export default function Home() {
       <Channels/>
       <Featured/>
       <Shows title="Action Movies"/>
-      <Shows title="Hindi Languauge Movies"/>
-      <Shows title="English Languauge Movies"/>
+      <Shows title="Hindi Movies"/>
+      <Shows title="English Movies"/>
       <Shows title="South Movies"/>
     </>
   )
